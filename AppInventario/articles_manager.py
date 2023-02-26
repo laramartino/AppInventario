4eee6a7bc5b8f068410544e00c361adda511b444
Matteo Gianferrari <93923514+matteogianferrari@users.noreply.github.com>
2023-02-26 22:53:14 +0100

    Migliorie su article_manager.py
    Migliorata la leggibilità del codice:
    -Corrette spaziature secondo standard (gli '='; scomposizione di tuple var1, var2 = tupla; dichiarazione di funzioni).
    -Standardizzate le docstring del modulo e della classe (usano tutti """).

--- AppInventario/articles_manager.py
+++ AppInventario/articles_manager.py
@@ -1,4 +1,4 @@
-""" Questo modulo contiene l'implementazione della classe ArticlesManager,
+"""Questo modulo contiene l'implementazione della classe ArticlesManager,
 una classe che rappresenta una struttura dati.
 
 Esempio:
@@ -15,29 +15,29 @@ Esempio:
 """
 
 class ArticlesManager:
-    ''' ArticlesManager e' una struttura dati che rappresenta il contenuto di un file inventario.
+    """ArticlesManager e' una struttura dati che rappresenta il contenuto di un file inventario.
 
     La struttura dati organizza i valori in un dizionario nel seguente modo:
-    - chiave (str): articolo 
-    - valore (list): lista di str che rappresentano le quantita'
+    - chiave (str): articolo.
+    - valore (list): lista di str che rappresentano le quantita'.
 
     Attributi:
-        dict_articoli (dict): dizionario che contiene le varie letture di tuple (articolo, quantita')
-    '''
+        dict_articoli (dict): dizionario che contiene le varie letture di tuple (articolo, quantita').
+    """
 
     def __init__(self):
-        '''Inizializza ArticlesManager'''
+        """Inizializza ArticlesManager."""
 
         self.dict_articoli = {}
 
     def inser_val(self, record: tuple) -> bool:
-        """ Data una tupla, la inserisce in ArticlesManager
+        """Data una tupla, inserisce il record in ArticlesManager.
 
         Arg:
-            record (tuple): tupla (articolo, quantita') da inserire
+            record (tuple): tupla (articolo, quantita') da inserire.
 
         Return:
-            bool: True se operazione effettuata correttamente
+            bool: True se operazione effettuata correttamente.
 
         Esempio:
             >>> obj= ArticlesManager()
@@ -45,23 +45,23 @@ class ArticlesManager:
             True    
         """
 
-        art,qty= record
+        art, qty = record
 
         if art not in self.dict_articoli:
-            self.dict_articoli[art]=[qty]
+            self.dict_articoli[art] = [qty]
         else:
             self.dict_articoli[art].append(qty)
 
         return True
 
-    def rimoz_val (self, record: tuple) -> bool:
-        """ Data una tupla, rimuove la quantita' da ArticlesManager
+    def rimoz_val(self, record: tuple) -> bool:
+        """Data una tupla, rimuove la quantita' del record da ArticlesManager.
 
         Arg:
-            record (tuple): tupla (articolo, quantita') da rimuovere
+            record (tuple): tupla (articolo, quantita') da rimuovere.
 
         Return:
-            bool: False se l'articolo non e' presente o se la quantita' non e' presente nell'articolo, altrimenti True 
+            bool: False se l'articolo non e' presente o se la quantita' non e' presente nell'articolo, altrimenti True.
 
         Esempio:
             >>> obj= ArticlesManager()
@@ -70,7 +70,7 @@ class ArticlesManager:
             True            
         """
 
-        art,qty= record
+        art ,qty = record
 
         if art not in self.dict_articoli:
             return False
@@ -82,14 +82,14 @@ class ArticlesManager:
                 del self.dict_articoli[art][pos]
                 return True
 
-    def modifica_val(self, record:tuple) -> bool:
-        """ Data una tupla, modifica una quantita' di un articolo gia' presente in ArticlesManager
+    def modifica_val(self, record: tuple) -> bool:
+        """Data una tupla, modifica una quantita' di un articolo gia' presente in ArticlesManager.
 
         Arg:
-            record (tuple): tupla (articolo, precedente_quantita', nuova_quantita')
+            record (tuple): tupla (articolo, precedente_quantita', nuova_quantita').
 
         Return:
-            bool: False se l'articolo non e' presente o se la precedente quantita' non e' presente nell'articolo, altrimenti True 
+            bool: False se l'articolo non e' presente o se la precedente quantita' non e' presente nell'articolo, altrimenti True.
 
         Esempio:
             >>> obj= ArticlesManager()
@@ -98,7 +98,7 @@ class ArticlesManager:
             True
         """
 
-        art,old_qty,new_qty= record
+        art, old_qty, new_qty = record
 
         if art not in self.dict_articoli:
             return False
@@ -107,23 +107,23 @@ class ArticlesManager:
                 return False
             else:
                 pos = self.dict_articoli[art].index(old_qty)
-                self.dict_articoli[art][pos]=new_qty
+                self.dict_articoli[art][pos] = new_qty
                 return True
 
     def insert_list_val(self, lista_val: list) -> bool:
-        '''Data una lista di tuple, inserisce tutti i valori in ArticlesManager
+        """Data una lista di tuple, inserisce tutti i record in ArticlesManager.
         
         Arg:
-            lista_val(list): lista di tuple (articolo, quantita') da inserire 
+            lista_val(list): lista di tuple (articolo, quantita') da inserire.
 
         Return:
-            bool: False se la lista e' vuota, altrimenti True
+            bool: False se la lista e' vuota, altrimenti True.
 
         Esempio:
             >>> obj= ArticlesManager()
             >>> obj.insert_list_val([('90515689','1000'),('90515689','500'),('90515689','300')])
             True            
-        '''
+        """
 
         if not lista_val: #lista vuota
             return False
@@ -131,5 +131,3 @@ class ArticlesManager:
         for record in lista_val:
             self.inser_val(record)
         return True
-
-
